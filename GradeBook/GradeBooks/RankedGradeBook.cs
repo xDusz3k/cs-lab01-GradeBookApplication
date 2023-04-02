e67aa250b10b5ec2df74ce4a0d4558912d9cdbaf
xDusz3k <michal124dyduch@gmail.com>
2023-04-02 18:21:13 +0200

    test4

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,5 +12,41 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            try
+            {
+                if (Students.Count<5)
+                {
+                    throw new InvalidOperationException();
+                    
+                }
+                if (averageGrade<=20)
+                {
+                    return 'F';
+                }
+                else if (averageGrade>20 && averageGrade<40)
+                {
+                    return 'D';
+                }
+                else if (averageGrade>40 && averageGrade <60)
+                {
+                    return 'C';
+                }
+                else if (averageGrade>60 && averageGrade <80)
+                {
+                    return 'B';
+                }
+                else
+                {
+                    return 'A';
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException();
+            }
+        }
     }
 }
