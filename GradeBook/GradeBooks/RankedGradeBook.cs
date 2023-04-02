8c3b9a48267005b1a574e87416bc77289353fc07
xDusz3k <michal124dyduch@gmail.com>
2023-04-02 18:34:52 +0200

    test5

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -48,5 +48,14 @@ namespace GradeBook.GradeBooks
                 throw new InvalidOperationException();
             }
         }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return ;
+            }            
+             base.CalculateStatistics();            
+        }
     }
 }
